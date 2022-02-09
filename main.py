9dbaf176b938fe92b57c229b430c6347140f7207
caioRafael <caiorafaelrg@gmail.com>
2022-02-08 23:43:26 -0300

    finalizando jogo da cobra

--- main.py
+++ main.py
@@ -15,11 +15,15 @@ barulho_colisao.set_volume(0.5)
 largura = 640
 altura = 480
 
-x = int(largura/2)
-y = int(altura/2)
+x_cobra = int(largura/2)
+y_cobra = int(altura/2)
 
-x_azul = randint(40, 600)
-y_azul = randint(50, 430)
+velocidade = 3
+x_controle = velocidade
+y_controle = 0
+
+x_maca = randint(40, 600)
+y_maca = randint(50, 430)
 
 pontos = 0
 fonte = pygame.font.SysFont("arial", 40, True, True)
@@ -30,34 +34,102 @@ pygame.display.set_caption('game')
 
 relogio = pygame.time.Clock()
 
+lista_cobra = []
+comprimento_inicial = 5
+
+morreu = False
+
+def aumenta_cobra(lista_cobra):
+    for XeY in lista_cobra:
+        pygame.draw.rect(tela,(0,255,0),(XeY[0], XeY[1], 20, 20))
+
+def reiniciar_jogo():
+    global pontos, x_cobra, y_cobra, lista_cabeca, lista_cobra, x_maca, y_maca, morreu, velocidade
+    pontos = 0
+    x_cobra = int(largura/2)
+    y_cobra = int(altura/2)
+    lista_cobra = []
+    lista_cabeca = []
+    x_maca = randint(40, 600)
+    y_maca = randint(50, 430)
+    morreu = False
+    velocidade = 3
+
 while True:
     relogio.tick(30)
-    tela.fill((0,0,0))
+    tela.fill((255,255,255))
     menssagem = (f'Prontos: {pontos}')
-    texto_formatado = fonte.render(menssagem, True, (255,255,255))
+    texto_formatado = fonte.render(menssagem, True, (0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
 
-    if pygame.key.get_pressed()[K_a]:
-        x = x - 10
-    if pygame.key.get_pressed()[K_d]:
-        x = x + 10
-    if pygame.key.get_pressed()[K_w]:
-        y = y - 10
-    if pygame.key.get_pressed()[K_s]:
-        y = y + 10
+        if event.type == KEYDOWN:
+            if event.key == K_a and x_controle <= 0:
+                x_controle = -velocidade
+                y_controle = 0
+            if event.key == K_d and x_controle >= 0:
+                x_controle = velocidade
+                y_controle = 0
+            if event.key == K_w and y_controle <= 0:
+                x_controle = 0
+                y_controle = -velocidade
+            if event.key == K_s and y_controle >= 0:
+                x_controle = 0
+                y_controle = velocidade
+                
+    x_cobra = x_cobra + x_controle
+    y_cobra = y_cobra + y_controle
 
+    cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra, y_cobra, 20, 20))
+    maca = pygame.draw.rect(tela, (255,0,0), (x_maca, y_maca, 20, 20))
 
-    ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x, y, 40, 50))
-    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul, y_azul, 40, 50))
+    lista_cabeca = []
 
-    if ret_vermelho.colliderect(ret_azul):
-        x_azul = randint(40, 600)
-        y_azul = randint(50, 430)
+    if cobra.colliderect(maca):
+        x_maca = randint(40, 600)
+        y_maca = randint(50, 430)
         pontos +=1
         barulho_colisao.play()
+        velocidade += 0.08
+
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+    lista_cobra.append(lista_cabeca) 
+
+    if lista_cobra.count(lista_cabeca) > 1:
+        fonte2 = pygame.font.SysFont('arial', 20, True, True)
+        menssagem = 'Game over! Pressione a tecla R para jogar novamente'
+        texto_formatado = fonte2.render(menssagem, True, (0,0,0))
+        retangulo_texto = texto_formatado.get_rect()
+        morreu = True
+        while morreu:
+            tela.fill((255,255,255))
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_r:
+                        reiniciar_jogo()
+            retangulo_texto.center = (largura//2, altura//2)
+            tela.blit(texto_formatado, retangulo_texto)
+            pygame.display.update()
+
+    if x_cobra > largura:
+        x_cobra = 0
+    if x_cobra < 0:
+        x_cobra = largura
+    if y_cobra > altura:
+        y_cobra = 0
+    if y_cobra < 0:
+        y_cobra = altura
+
+    if len(lista_cobra) > pontos+1:
+        del lista_cobra[0]
+
+    aumenta_cobra(lista_cobra)
 
     tela.blit(texto_formatado, (40, 40))
 
